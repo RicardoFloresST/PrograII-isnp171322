a8bbd6032638526eea297797a3a07e89d78d3656
RICARDO FLORES ECHEVERRIA <ISNP171322@ugb.edu.sv>
2024-04-26 21:58:15 -0600

    Ejercicios de tarea unidad 1 y 2

---
+++ ISNP171322_Unidad_2/Ejercicio_2/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace Ejercicio_2
+{
+    partial class ConversorLongForms
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ConversorLongForms));
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbOrigen = new System.Windows.Forms.ComboBox();
+            this.cmbdestino = new System.Windows.Forms.ComboBox();
+            this.texCant = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnConver = new System.Windows.Forms.Button();
+            this.texReslut = new System.Windows.Forms.TextBox();
+            this.btnclear = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("BankGothic Md BT", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(76, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(465, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Bienvenidos al conversor de Longitud";
+            // 
+            // cmbOrigen
+            // 
+            this.cmbOrigen.AllowDrop = true;
+            this.cmbOrigen.FormattingEnabled = true;
+            this.cmbOrigen.Location = new System.Drawing.Point(244, 236);
+            this.cmbOrigen.Name = "cmbOrigen";
+            this.cmbOrigen.Size = new System.Drawing.Size(121, 21);
+            this.cmbOrigen.TabIndex = 1;
+            // 
+            // cmbdestino
+            // 
+            this.cmbdestino.FormattingEnabled = true;
+            this.cmbdestino.Location = new System.Drawing.Point(245, 315);
+            this.cmbdestino.Name = "cmbdestino";
+            this.cmbdestino.Size = new System.Drawing.Size(121, 21);
+            this.cmbdestino.TabIndex = 2;
+            // 
+            // texCant
+            // 
+            this.texCant.Location = new System.Drawing.Point(245, 171);
+            this.texCant.Name = "texCant";
+            this.texCant.Size = new System.Drawing.Size(121, 20);
+            this.texCant.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(206, 138);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(201, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Ingrese la cantidad a convertir";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold);
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(290, 211);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(25, 15);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "De";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold);
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(295, 289);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(15, 15);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "A";
+            // 
+            // btnConver
+            // 
+            this.btnConver.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnConver.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConver.ForeColor = System.Drawing.SystemColors.Info;
+            this.btnConver.Location = new System.Drawing.Point(266, 355);
+            this.btnConver.Name = "btnConver";
+            this.btnConver.Size = new System.Drawing.Size(75, 23);
+            this.btnConver.TabIndex = 8;
+            this.btnConver.Text = "Convertir";
+            this.btnConver.UseVisualStyleBackColor = false;
+            this.btnConver.Click += new System.EventHandler(this.btnconver_Click);
+            // 
+            // texReslut
+            // 
+            this.texReslut.Location = new System.Drawing.Point(245, 418);
+            this.texReslut.Name = "texReslut";
+            this.texReslut.Size = new System.Drawing.Size(121, 20);
+            this.texReslut.TabIndex = 9;
+            // 
+            // btnclear
+            // 
+            this.btnclear.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnclear.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnclear.ForeColor = System.Drawing.SystemColors.Info;
+            this.btnclear.Location = new System.Drawing.Point(266, 455);
+            this.btnclear.Name = "btnclear";
+            this.btnclear.Size = new System.Drawing.Size(75, 23);
+            this.btnclear.TabIndex = 10;
+            this.btnclear.Text = "Limpiar";
+            this.btnclear.UseVisualStyleBackColor = false;
+            this.btnclear.Click += new System.EventHandler(this.btnclear_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.SteelBlue;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(490, 552);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Salir";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.btnexit_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.ForeColor = System.Drawing.SystemColors.ScrollBar;
+            this.label6.Location = new System.Drawing.Point(483, 612);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(134, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Powered by Ricardo Flores";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold);
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(269, 398);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(72, 15);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Resultado";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::Ejercicio_2.Properties.Resources.Logotipo_vertical_white120x97;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 520);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(129, 105);
+            this.pictureBox1.TabIndex = 14;
+            this.pictureBox1.TabStop = false;
+            // 
+            // ConversorLongForms
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = global::Ejercicio_2.Properties.Resources.wp6734562;
+            this.ClientSize = new System.Drawing.Size(616, 627);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnclear);
+            this.Controls.Add(this.texReslut);
+            this.Controls.Add(this.btnConver);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.texCant);
+            this.Controls.Add(this.cmbdestino);
+            this.Controls.Add(this.cmbOrigen);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "ConversorLongForms";
+            this.Text = "Conversor UGB";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbOrigen;
+        private System.Windows.Forms.ComboBox cmbdestino;
+        private System.Windows.Forms.TextBox texCant;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnConver;
+        private System.Windows.Forms.TextBox texReslut;
+        private System.Windows.Forms.Button btnclear;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ ISNP171322_Unidad_2/Ejercicio_2/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;//libreria usada para validar los textbox
+using System.Runtime.InteropServices;//libreria usada para mover el form
+
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Ejercicio_2
+{
+    public partial class ConversorLongForms : Form
+    {
+        
+        public ConversorLongForms()
+        {
+            InitializeComponent();
+            
+            // Llenar el ComboBox con las opciones de longitud
+            cmbOrigen.Items.Add("Metros");
+            cmbOrigen.Items.Add("Yardas");
+            cmbOrigen.Items.Add("Centímetros");
+            cmbOrigen.Items.Add("Milímetros");
+            
+            // Seleccionar la primera opción por defecto
+            cmbOrigen.SelectedIndex = 0;
+
+            // Llenar el ComboBox con las opciones de longitud
+            cmbdestino.Items.Add("Metros");
+            cmbdestino.Items.Add("Yardas");
+            cmbdestino.Items.Add("Centímetros");
+            cmbdestino.Items.Add("Milímetros");
+
+            // Seleccionar la primera opción por defecto
+            cmbdestino.SelectedIndex = 1;
+        }
+
+        private void RealizarConversion(string unidadResultado)
+        {
+            
+            try
+            {
+               
+                double valor = double.Parse(texCant.Text);
+            double resultado = 0.0;
+            
+
+                // Realizar la conversión basada en las unidades seleccionadas en los ComboBoxes
+                if (cmbOrigen.SelectedIndex == 0 && cmbdestino.SelectedIndex == 1) // Metros a Yardas
+            {
+                resultado = valor * 1.09361;
+                    unidadResultado = "yrd";
+                }
+            else if (cmbOrigen.SelectedIndex == 1 && cmbdestino.SelectedIndex == 0) // Yardas a Metros
+            {
+                resultado = valor / 1.09361;
+                    unidadResultado = "mt";
+                }
+            else if (cmbOrigen.SelectedIndex == 0 && cmbdestino.SelectedIndex == 2) // Metros a Centímetros
+            {
+                resultado = valor * 100;
+                    unidadResultado = "cm";
+                }
+            else if (cmbOrigen.SelectedIndex == 2 && cmbdestino.SelectedIndex == 0) // Centímetros a Metros
+            {
+                resultado = valor / 100;
+                    unidadResultado = "mt";
+                }
+            else if (cmbOrigen.SelectedIndex == 0 && cmbdestino.SelectedIndex == 3) // Metros a Milímetros
+            {
+                resultado = valor * 1000;
+                    unidadResultado = "mm";
+                }
+            else if (cmbOrigen.SelectedIndex == 3 && cmbdestino.SelectedIndex == 0) // Milímetros a Metros
+            {
+                resultado = valor / 1000;
+                    unidadResultado = "mt";
+
+                }
+
+            else if (cmbOrigen.SelectedIndex == 1 && cmbdestino.SelectedIndex == 3) // Yardas a Milímetros
+            {
+                resultado = valor * 914.4;
+                    unidadResultado = "mm";
+                }
+
+            else if (cmbOrigen.SelectedIndex == 3 && cmbdestino.SelectedIndex == 1) // Milímetros a Yardas
+            {
+                resultado = valor / 914.4;
+                    unidadResultado = "yrd";
+                }
+
+            else if (cmbOrigen.SelectedIndex == 2 && cmbdestino.SelectedIndex == 3) // Centímetros a Milímetros
+            {
+                resultado = valor * 10;
+                    unidadResultado = "mm";
+                }
+
+            else if (cmbOrigen.SelectedIndex == 1 && cmbdestino.SelectedIndex == 2) // Yardas a Centímetros
+            {
+                resultado = valor * 91.44;
+                    unidadResultado = "cm";
+                }
+
+            else if (cmbOrigen.SelectedIndex == 2 && cmbdestino.SelectedIndex == 1) // Yardas a Centímetros
+            {
+                resultado = valor / 91.44;
+                    unidadResultado = "cm";
+                }
+
+            else if (cmbOrigen.SelectedIndex == 3 && cmbdestino.SelectedIndex == 2) // Milímetros a centímetros
+            {
+                resultado = valor / 10;
+                    unidadResultado = "cm";
+                }
+
+                // Agregar más condiciones según sea necesario para otras conversiones
+                resultado = Math.Round(resultado, 4);
+                texReslut.Text = resultado.ToString() + " " + unidadResultado;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error por favor ingrese los valores solicitados", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void texCant_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Verificar si la tecla presionada no es un número o una tecla de control
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                // Mostrar una advertencia al usuario
+                MessageBox.Show("Por favor, ingresa solo números.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                // Detener la tecla presionada para que no se escriba en el TextBox
+                e.Handled = true;
+            }
+        }
+
+        private void btnconver_Click(object sender, EventArgs e)
+         {
+             RealizarConversion(texReslut.Text);
+           
+        }
+
+        private void btnclear_Click(object sender, EventArgs e)
+        {
+            //usado para dejar en blanco los textbox
+            texCant.Text = "";
+            texReslut.Text = "";
+        }
+
+        private void btnexit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        
+         private void label6_Click(object sender, EventArgs e)
+         {
+            AboutBox1 formAcercaDe = new AboutBox1();
+            formAcercaDe.ShowDialog();
+         }
+
+       }
+    }
+ 
+
+
+
+
+
