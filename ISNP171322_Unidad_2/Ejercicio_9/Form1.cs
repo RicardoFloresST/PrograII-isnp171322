1ca1ec4771b3703eca8b4da1c8ee18f1fe142d36
RICARDO FLORES ECHEVERRIA <ISNP171322@ugb.edu.sv>
2024-04-28 12:06:32 -0600

    Ejercicio_9

---
+++ ISNP171322_Unidad_2/Ejercicio_9/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio_9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            // Array para almacenar los números ingresados por el usuario
+            double[] numeros = new double[10];
+
+            // Obtener los números ingresados por el usuario y almacenarlos en el array
+            for (int i = 0; i < 10; i++)
+            {
+                if (!double.TryParse(Controls[$"txtNumero{i + 1}"].Text, out numeros[i]))
+                {
+                    MessageBox.Show("Por favor, ingrese números válidos en todos los campos.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+            }
+
+            // Encontrar el número mayor en el array
+            double mayor = numeros.Max();
+
+            // Mostrar el número mayor en el Label
+            lblResult.Text = $"El mayor de los 10 números es: {mayor}";
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("¡Gracias por usar este programa!", "¡Hasta luego!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            this.Close(); // Cierra la aplicación
+        }
+    }
+}
+        
+    
+
