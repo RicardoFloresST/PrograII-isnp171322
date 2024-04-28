40cbf3b1ef4651c7cfdab528e9e680d5b9ccaaf2
RICARDO FLORES ECHEVERRIA <ISNP171322@ugb.edu.sv>
2024-04-27 22:29:48 -0600

    Ejercicio_5

--- ISNP171322_Unidad_2/Ejercicio_5/Form1.Designer.cs
+++ ISNP171322_Unidad_2/Ejercicio_5/Form1.Designer.cs
@@ -28,13 +28,219 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblLap1 = new System.Windows.Forms.Label();
+            this.lblLap2 = new System.Windows.Forms.Label();
+            this.lblLap3 = new System.Windows.Forms.Label();
+            this.txtLab1 = new System.Windows.Forms.TextBox();
+            this.txtLab2 = new System.Windows.Forms.TextBox();
+            this.txtLab3 = new System.Windows.Forms.TextBox();
+            this.txtPar3 = new System.Windows.Forms.TextBox();
+            this.txtPar2 = new System.Windows.Forms.TextBox();
+            this.txtPar1 = new System.Windows.Forms.TextBox();
+            this.lblPar3 = new System.Windows.Forms.Label();
+            this.lblPar2 = new System.Windows.Forms.Label();
+            this.lblPar1 = new System.Windows.Forms.Label();
+            this.btnPromedio = new System.Windows.Forms.Button();
+            this.txtNotaFin = new System.Windows.Forms.TextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblLap1
+            // 
+            this.lblLap1.AutoSize = true;
+            this.lblLap1.Location = new System.Drawing.Point(32, 113);
+            this.lblLap1.Name = "lblLap1";
+            this.lblLap1.Size = new System.Drawing.Size(69, 13);
+            this.lblLap1.TabIndex = 0;
+            this.lblLap1.Text = "Laboratorio 1";
+            // 
+            // lblLap2
+            // 
+            this.lblLap2.AutoSize = true;
+            this.lblLap2.Location = new System.Drawing.Point(32, 142);
+            this.lblLap2.Name = "lblLap2";
+            this.lblLap2.Size = new System.Drawing.Size(69, 13);
+            this.lblLap2.TabIndex = 1;
+            this.lblLap2.Text = "Laboratorio 2";
+            // 
+            // lblLap3
+            // 
+            this.lblLap3.AutoSize = true;
+            this.lblLap3.Location = new System.Drawing.Point(32, 175);
+            this.lblLap3.Name = "lblLap3";
+            this.lblLap3.Size = new System.Drawing.Size(69, 13);
+            this.lblLap3.TabIndex = 2;
+            this.lblLap3.Text = "Laboratorio 3";
+            // 
+            // txtLab1
+            // 
+            this.txtLab1.Location = new System.Drawing.Point(121, 111);
+            this.txtLab1.Name = "txtLab1";
+            this.txtLab1.Size = new System.Drawing.Size(35, 20);
+            this.txtLab1.TabIndex = 3;
+            // 
+            // txtLab2
+            // 
+            this.txtLab2.Location = new System.Drawing.Point(121, 142);
+            this.txtLab2.Name = "txtLab2";
+            this.txtLab2.Size = new System.Drawing.Size(35, 20);
+            this.txtLab2.TabIndex = 4;
+            // 
+            // txtLab3
+            // 
+            this.txtLab3.Location = new System.Drawing.Point(121, 175);
+            this.txtLab3.Name = "txtLab3";
+            this.txtLab3.Size = new System.Drawing.Size(35, 20);
+            this.txtLab3.TabIndex = 5;
+            // 
+            // txtPar3
+            // 
+            this.txtPar3.Location = new System.Drawing.Point(308, 175);
+            this.txtPar3.Name = "txtPar3";
+            this.txtPar3.Size = new System.Drawing.Size(35, 20);
+            this.txtPar3.TabIndex = 11;
+            // 
+            // txtPar2
+            // 
+            this.txtPar2.Location = new System.Drawing.Point(308, 142);
+            this.txtPar2.Name = "txtPar2";
+            this.txtPar2.Size = new System.Drawing.Size(35, 20);
+            this.txtPar2.TabIndex = 10;
+            // 
+            // txtPar1
+            // 
+            this.txtPar1.Location = new System.Drawing.Point(308, 111);
+            this.txtPar1.Name = "txtPar1";
+            this.txtPar1.Size = new System.Drawing.Size(35, 20);
+            this.txtPar1.TabIndex = 9;
+            // 
+            // lblPar3
+            // 
+            this.lblPar3.AutoSize = true;
+            this.lblPar3.Location = new System.Drawing.Point(235, 175);
+            this.lblPar3.Name = "lblPar3";
+            this.lblPar3.Size = new System.Drawing.Size(48, 13);
+            this.lblPar3.TabIndex = 8;
+            this.lblPar3.Text = "Parcial 3";
+            // 
+            // lblPar2
+            // 
+            this.lblPar2.AutoSize = true;
+            this.lblPar2.Location = new System.Drawing.Point(235, 142);
+            this.lblPar2.Name = "lblPar2";
+            this.lblPar2.Size = new System.Drawing.Size(48, 13);
+            this.lblPar2.TabIndex = 7;
+            this.lblPar2.Text = "Parcial 2";
+            // 
+            // lblPar1
+            // 
+            this.lblPar1.AutoSize = true;
+            this.lblPar1.Location = new System.Drawing.Point(235, 113);
+            this.lblPar1.Name = "lblPar1";
+            this.lblPar1.Size = new System.Drawing.Size(48, 13);
+            this.lblPar1.TabIndex = 6;
+            this.lblPar1.Text = "Parcial 1";
+            // 
+            // btnPromedio
+            // 
+            this.btnPromedio.Location = new System.Drawing.Point(99, 239);
+            this.btnPromedio.Name = "btnPromedio";
+            this.btnPromedio.Size = new System.Drawing.Size(130, 32);
+            this.btnPromedio.TabIndex = 12;
+            this.btnPromedio.Text = "Obtener Promedio";
+            this.btnPromedio.UseVisualStyleBackColor = true;
+            this.btnPromedio.Click += new System.EventHandler(this.btnPromedio_Click);
+            // 
+            // txtNotaFin
+            // 
+            this.txtNotaFin.Location = new System.Drawing.Point(261, 245);
+            this.txtNotaFin.Name = "txtNotaFin";
+            this.txtNotaFin.Size = new System.Drawing.Size(100, 20);
+            this.txtNotaFin.TabIndex = 13;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Ejercicio_5.Properties.Resources.Logotipo_vertical_azul1;
+            this.pictureBox2.Location = new System.Drawing.Point(2, 349);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(124, 114);
+            this.pictureBox2.TabIndex = 15;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Ejercicio_5.Properties.Resources.ingefondo1;
+            this.pictureBox1.Location = new System.Drawing.Point(367, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(438, 447);
+            this.pictureBox1.TabIndex = 14;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(35, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(146, 22);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Programación II";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtNotaFin);
+            this.Controls.Add(this.btnPromedio);
+            this.Controls.Add(this.txtPar3);
+            this.Controls.Add(this.txtPar2);
+            this.Controls.Add(this.txtPar1);
+            this.Controls.Add(this.lblPar3);
+            this.Controls.Add(this.lblPar2);
+            this.Controls.Add(this.lblPar1);
+            this.Controls.Add(this.txtLab3);
+            this.Controls.Add(this.txtLab2);
+            this.Controls.Add(this.txtLab1);
+            this.Controls.Add(this.lblLap3);
+            this.Controls.Add(this.lblLap2);
+            this.Controls.Add(this.lblLap1);
+            this.Name = "Form1";
+            this.Text = "Calculando Promedio";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblLap1;
+        private System.Windows.Forms.Label lblLap2;
+        private System.Windows.Forms.Label lblLap3;
+        private System.Windows.Forms.TextBox txtLab1;
+        private System.Windows.Forms.TextBox txtLab2;
+        private System.Windows.Forms.TextBox txtLab3;
+        private System.Windows.Forms.TextBox txtPar3;
+        private System.Windows.Forms.TextBox txtPar2;
+        private System.Windows.Forms.TextBox txtPar1;
+        private System.Windows.Forms.Label lblPar3;
+        private System.Windows.Forms.Label lblPar2;
+        private System.Windows.Forms.Label lblPar1;
+        private System.Windows.Forms.Button btnPromedio;
+        private System.Windows.Forms.TextBox txtNotaFin;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
